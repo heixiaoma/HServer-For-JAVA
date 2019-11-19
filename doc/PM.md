87d0e6c998a62807156942ad8df6b0d014592e62
黑小马 <heixiaoma@gmail.com>
2019-11-19 12:04:24 +0800

    模板引擎实现，和Linux环境下压测

--- doc/PM.md
+++ doc/PM.md
@@ -1,9 +1,23 @@
 
-### 环境
-    win10,i7_9700k，真实服务器压测，改天补充
+## 压测
+
+    通常不可能跑一个hello程序，都会伴随着业务，压测截图只是参考，框架qps本身很高,就看你怎么玩了
+
+### 环境Win（Nio）
+    win10,i7_9700k(8核16G)
 ##### 1.压测json
 ![AB测试](https://gitee.com/heixiaomas_admin/HServer/raw/master/doc/json.png)
 ##### 2.压测静态文件
 ![AB测试](https://gitee.com/heixiaomas_admin/HServer/raw/master/doc/file.png)
 ##### 3.模板引擎html
 ![AB测试](https://gitee.com/heixiaomas_admin/HServer/raw/master/doc/template.png)
+
+
+### 环境Linux（Epoll）
+    阿里云学生机 Centos7.3（1核2G内存）
+##### 1.压测json
+![AB测试](https://gitee.com/heixiaomas_admin/HServer/raw/master/doc/LinuxJson.png)
+##### 2.压测静态文件
+![AB测试](https://gitee.com/heixiaomas_admin/HServer/raw/master/doc/LinuxFile.png)
+##### 3.模板引擎html
+![AB测试](https://gitee.com/heixiaomas_admin/HServer/raw/master/doc/LinuxTemp.png)
--- src/main/java/com/hserver/core/server/util/FreemarkerUtil.java
+++ src/main/java/com/hserver/core/server/util/FreemarkerUtil.java
@@ -1,17 +1,27 @@
 package com.hserver.core.server.util;
 
+import freemarker.cache.ClassTemplateLoader;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateExceptionHandler;
 import lombok.extern.slf4j.Slf4j;
-import java.io.File;
 import java.io.StringWriter;
 import java.util.Map;
+
 
 @Slf4j
 public class FreemarkerUtil {
 
     private static final Configuration cfg = new Configuration(Configuration.VERSION_2_3_27);
+
+    static {
+        cfg.setClassForTemplateLoading(FreemarkerUtil.class, "/template");
+        cfg.setTemplateLoader(new ClassTemplateLoader(FreemarkerUtil.class, "/template"));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+        cfg.setWrapUncheckedExceptions(true);
+    }
 
     /**
      * 获取模板内容
@@ -19,20 +29,15 @@ public class FreemarkerUtil {
      * @param template 模板文件
      * @param map      模板参数
      * @return 渲染后的模板内容
-     * @throws Exception       Exception
+     * @throws Exception Exception
      */
     public static String getTemplate(String template, Map map) throws Exception {
-        String templatePath = FreemarkerUtil.class.getResource("/template/").getPath();
-//        log.info("FreemarkerUtil template  = " + template);
-//        log.debug("templatePath = " + templatePath);
-        cfg.setDirectoryForTemplateLoading(new File(templatePath));
-        cfg.setDefaultEncoding("UTF-8");
-        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-        cfg.setLogTemplateExceptions(false);
-        cfg.setWrapUncheckedExceptions(true);
+
+
         Template temp = cfg.getTemplate(template);
         StringWriter stringWriter = new StringWriter();
         temp.process(map, stringWriter);
         return stringWriter.toString();
     }
+
 }
