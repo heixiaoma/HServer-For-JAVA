370f3fdfdf63d7e6151c3403f5819afb3d81c8a7
黑小马 <heixiaoma@gmail.com>
2020-04-22 22:20:15 +0800

    链路跟踪Bug修复

--- src/main/java/top/hserver/core/ioc/ref/MemoryInitClass.java
+++ src/main/java/top/hserver/core/ioc/ref/MemoryInitClass.java
@@ -3,6 +3,7 @@ package top.hserver.core.ioc.ref;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtMethod;
+import lombok.extern.slf4j.Slf4j;
 import top.hserver.core.interfaces.TrackAdapter;
 import top.hserver.core.ioc.annotation.Track;
 import top.hserver.core.server.util.JavassistClassLoadUtil;
@@ -15,6 +16,7 @@ import java.util.List;
  * @author hxm
  * 有些操作需要处理内存处理哈
  */
+@Slf4j
 public class MemoryInitClass {
 
     public static void init(Class clazz) {
@@ -25,11 +27,20 @@ public class MemoryInitClass {
             List<Class<?>> classes = JavassistClassLoadUtil.LoadClasses(clazz.getPackage().getName(), true);
             for (Class<?> aClass : classes) {
                 Method[] methods = aClass.getMethods();
-                for (Method method : methods) {
+                bname:for (Method method : methods) {
                     Annotation[] annotations = method.getAnnotations();
                     for (Annotation annotation : annotations) {
                         if (annotation.annotationType().getName().equals(Track.class.getName())) {
-                            initTrack(aClass, method);
+                            log.debug("被链路跟踪的类：{}",aClass.getName());
+                            ClassPool cp = ClassPool.getDefault();
+                            CtClass cc = cp.get(aClass.getName());
+                            cc.freeze();
+                            cc.defrost();
+                            //跟踪处理
+                            initTrack(cc, cp);
+                            //带扩展的的功能。。。。
+                            cc.toClass();
+                            break bname;
                         }
                     }
                 }
@@ -40,60 +51,41 @@ public class MemoryInitClass {
     }
 
 
-    private static void initTrack(Class aClass, Method method) throws Exception {
-        try {
-            ClassPool cp = ClassPool.getDefault();
-            CtClass cc = cp.get(aClass.getName());
-            cc.freeze();
-            cc.defrost();
-            name:
-            for (CtMethod declaredMethod : cc.getDeclaredMethods()) {
-                //方法名字相同
-                if (declaredMethod.getName().equals(method.getName())) {
-                    //参数长度相同
-                    if (declaredMethod.getParameterTypes().length == method.getParameterTypes().length) {
-                        //每一位类型也一样
-                        int size = declaredMethod.getParameterTypes().length;
-                        for (int i = 0; i < size; i++) {
-                            if (!declaredMethod.getParameterTypes()[i].getSimpleName().equals(method.getParameterTypes()[i])) {
-                                break name;
-                            }
-                        }
-                        declaredMethod.addLocalVariable("begin_hserver", CtClass.longType);
-                        declaredMethod.addLocalVariable("end_hserver", CtClass.longType);
-                        declaredMethod.addLocalVariable("trackAdapter_hserver", cp.get(TrackAdapter.class.getCanonicalName()));
-                        declaredMethod.addLocalVariable("clazz_hserver", cp.get(Class.class.getCanonicalName()));
-                        declaredMethod.addLocalVariable("method_hserver", cp.get(Method.class.getCanonicalName()));
-                        declaredMethod.insertBefore("begin_hserver=System.currentTimeMillis();");
+    private static void initTrack(CtClass cc,ClassPool cp) throws Exception {
+        CtMethod[] methods = cc.getMethods();
+        for (CtMethod declaredMethod : methods) {
+            Object annotation = declaredMethod.getAnnotation(Track.class);
+            if (annotation!=null){
+                log.debug("被链路跟踪的方法：{}",declaredMethod.getName());
+                declaredMethod.addLocalVariable("begin_hserver", CtClass.longType);
+                declaredMethod.addLocalVariable("end_hserver", CtClass.longType);
+                declaredMethod.addLocalVariable("trackAdapter_hserver", cp.get(TrackAdapter.class.getCanonicalName()));
+                declaredMethod.addLocalVariable("clazz_hserver", cp.get(Class.class.getCanonicalName()));
+                declaredMethod.addLocalVariable("method_hserver", cp.get(Method.class.getCanonicalName()));
+                declaredMethod.insertBefore("begin_hserver=System.currentTimeMillis();");
 
-
-                        StringBuilder src = new StringBuilder();
-                        src.append("end_hserver=System.currentTimeMillis();");
-                        src.append("trackAdapter_hserver = top.hserver.core.ioc.IocUtil.getBean(top.hserver.core.interfaces.TrackAdapter.class);");
-                        if (!Modifier.isStatic(method.getModifiers())) {
-                            //非静态
-                            src.append("clazz_hserver = this.getClass();");
-                        } else {
-                            //静态
-                            src.append("clazz_hserver = " + aClass.getName() + ".class;");
-                        }
-                        src.append("if (trackAdapter_hserver!=null)");
-                        src.append("{");
-                        src.append(" trackAdapter_hserver.track(clazz_hserver,Thread.currentThread().getStackTrace(), begin_hserver,end_hserver);");
-                        src.append("}");
-                        src.append("else");
-                        src.append("{");
-                        src.append("System.out.println(\"请实现，TrackAdapter接口，并用@Bean标注\");");
-                        src.append("}");
-                        declaredMethod.insertAfter(src.toString());
-                        cc.toClass();
-                    }
+                StringBuilder src = new StringBuilder();
+                src.append("end_hserver=System.currentTimeMillis();");
+                src.append("trackAdapter_hserver = top.hserver.core.ioc.IocUtil.getBean(top.hserver.core.interfaces.TrackAdapter.class);");
+                if (!Modifier.isStatic(declaredMethod.getModifiers())) {
+                    //非静态
+                    src.append("clazz_hserver = this.getClass();");
+                } else {
+                    //静态
+                    src.append("clazz_hserver = " + cc.getName() + ".class;");
                 }
+                src.append("if (trackAdapter_hserver!=null)");
+                src.append("{");
+                src.append(" trackAdapter_hserver.track(clazz_hserver,Thread.currentThread().getStackTrace(), begin_hserver,end_hserver);");
+                src.append("}");
+                src.append("else");
+                src.append("{");
+                src.append("System.out.println(\"请实现，TrackAdapter接口，并用@Bean标注\");");
+                src.append("}");
+                declaredMethod.insertAfter(src.toString());
             }
-        } catch (
-                Exception e) {
-            e.printStackTrace();
         }
+
     }
 
 }
--- src/main/java/top/test/track/TrackImpl.java
+++ src/main/java/top/test/track/TrackImpl.java
@@ -3,6 +3,7 @@ package top.test.track;
 import lombok.extern.slf4j.Slf4j;
 import top.hserver.core.interfaces.TrackAdapter;
 import top.hserver.core.ioc.annotation.Bean;
+import top.hserver.core.server.util.JvmStack;
 
 /**
  * @author hxm
@@ -13,5 +14,7 @@ public class TrackImpl implements TrackAdapter {
     @Override
     public void track(Class clazz,StackTraceElement[] stackTraceElements, long start, long end) throws Exception {
         log.info("当前类：{},当前方法：{},耗时：{}", clazz.getName(), stackTraceElements[1].getMethodName(), (end - start) + "ms");
+        JvmStack.printMemoryInfo();
+        JvmStack.printGCInfo();
     }
 }
