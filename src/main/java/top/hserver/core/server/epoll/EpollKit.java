36fe95177dc1ffa1a08017e5bf6f7c6a2c71dd9b
黑小马 <heixiaoma@gmail.com>
2020-05-08 18:20:05 +0800

    优化代码更新文档

--- src/main/java/top/hserver/core/server/epoll/EpollKit.java
+++ src/main/java/top/hserver/core/server/epoll/EpollKit.java
@@ -2,13 +2,12 @@ package top.hserver.core.server.epoll;
 
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.epoll.EpollServerSocketChannel;
-import lombok.var;
 
 public class EpollKit {
 
   public static NettyServerGroup group(int threadCount, int workers, String name) {
-    var bossGroup = new EpollEventLoopGroup(threadCount, new NamedThreadFactory(name + "_epoll_boss@"));
-    var workerGroup = new EpollEventLoopGroup(workers, new NamedThreadFactory(name + "_epoll_worker@"));
+    EpollEventLoopGroup bossGroup = new EpollEventLoopGroup(threadCount, new NamedThreadFactory(name + "_epoll_boss@"));
+    EpollEventLoopGroup workerGroup = new EpollEventLoopGroup(workers, new NamedThreadFactory(name + "_epoll_worker@"));
     return NettyServerGroup.builder().boosGroup(bossGroup).workerGroup(workerGroup).socketChannel(EpollServerSocketChannel.class).build();
   }
 
