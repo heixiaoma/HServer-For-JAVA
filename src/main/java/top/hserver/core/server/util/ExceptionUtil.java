66338153445777c4a2e4078ee7645bec32da3eb2
黑小马 <heixiaoma@gmail.com>
2020-06-13 11:15:33 +0800

    :tada: 自定义banner，发布2.9.22

--- src/main/java/top/hserver/core/server/util/ExceptionUtil.java
+++ src/main/java/top/hserver/core/server/util/ExceptionUtil.java
@@ -5,27 +5,36 @@ import java.io.StringWriter;
 
 public class ExceptionUtil {
 
-    private static Throwable getExceptionType(Exception e) {
+    private static Throwable getExceptionType(Throwable e) {
         return e;
     }
 
-    private static String getExceptionMessage(Exception e) {
+    private static String getExceptionMessage(Throwable e) {
         return e.getMessage();
     }
 
-    private static String getExceptionSrintStackTrace(Exception e) {
+    private static String getExceptionSrintStackTrace(Throwable e) {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
         e.printStackTrace(pw);
         return sw.toString();
     }
 
-    public static String getMessage(Exception e) {
+    public static String getMessage(Throwable e) {
         StringBuilder stringBuilder = new StringBuilder();
 //        stringBuffer.append("\nat " + ExceptionUtil.getExceptionType(e));
         stringBuilder.append("\n").append(ExceptionUtil.getExceptionSrintStackTrace(e));
         if (e.getMessage() != null) {
             stringBuilder.append("\nat ").append(ExceptionUtil.getExceptionMessage(e));
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String getHtmlMessage(Throwable e) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("<br>").append(getExceptionSrintStackTrace(e).replaceAll("\n", "<br>").replaceAll("\t", "&nbsp;&nbsp;&nbsp;&nbsp;"));
+        if (e.getMessage() != null) {
+            stringBuilder.append("<br>&nbsp;&nbsp;&nbsp;&nbsp;at ").append(getExceptionMessage(e));
         }
         return stringBuilder.toString();
     }
