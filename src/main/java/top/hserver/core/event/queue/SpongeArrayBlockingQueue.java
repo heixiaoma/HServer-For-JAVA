af9fd549ba2923bdb05e2c45f1d4c5e6a17c2bf8
424352644 <424352644@qq.com>
2020-06-28 10:39:32 +0800

    升级fastjson

--- src/main/java/top/hserver/core/event/queue/SpongeArrayBlockingQueue.java
+++ src/main/java/top/hserver/core/event/queue/SpongeArrayBlockingQueue.java
@@ -4,7 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import lombok.extern.slf4j.Slf4j;
-import top.hserver.cloud.util.SerializationUtil;
 import top.hserver.core.event.queue.util.DataByteArrayOutputStream;
 import top.hserver.core.event.queue.util.Utilities;
 
