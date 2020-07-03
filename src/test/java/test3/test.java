43b004f28205e84b32352d00fcd9f075885db04a
黑小马 <heixiaoma@gmail.com>
2020-07-03 20:31:36 +0800

    :zap: ctx使用案例

--- src/test/java/test3/test.java
+++ src/test/java/test3/test.java
@@ -2,6 +2,7 @@ package test3;
 
 import top.hserver.core.server.handlers.StaticHandler;
 import top.hserver.core.server.util.EnvironmentUtil;
+import top.hserver.core.server.util.JvmStack;
 
 import java.io.File;
 import java.net.URL;
@@ -12,7 +13,7 @@ import java.util.concurrent.ConcurrentHashMap;
 public class test {
     public static void main(String[] args) {
 //        EnvironmentUtil.init();
-
+        JvmStack.printMemoryInfo();
     }
 
 
