2e5a13febab5560fccf9db675bdbbe68f4ba1177
黑小马 <heixiaoma@gmail.com>
2020-08-24 20:16:02 +0800

    :zap: 发布2.9.45,支持自定义业务线程

--- src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -33,7 +33,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.44";
+    public static final String VERSION = "2.9.45";
     /**
      * 定时任务线程数配置
      */
