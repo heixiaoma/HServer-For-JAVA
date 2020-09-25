4173d32306076666f8b6046bc4d339e2a76e6610
424352644 <424352644@qq.com>
2020-09-25 18:32:48 +0800

    优化调整Http解码器问题

--- src/main/java/top/hserver/core/server/handlers/HServerContentHandler.java
+++ src/main/java/top/hserver/core/server/handlers/HServerContentHandler.java
@@ -32,28 +32,10 @@ public class HServerContentHandler extends SimpleChannelInboundHandler<FullHttpR
         request.setPort(HServerIpUtil.getClientPort(channelHandlerContext));
         request.setCtx(channelHandlerContext);
         request.setNettyUri(req.uri());
-        DefaultFullHttpRequest defaultFullHttpRequest;
         hServerContext.setCtx(channelHandlerContext);
-        if (req.method() == HttpMethod.GET) {
-            Map<String, String> requestParams = new HashMap<>(1);
-            QueryStringDecoder decoder = new QueryStringDecoder(req.uri());
-            Map<String, List<String>> params = decoder.parameters();
-            for (Map.Entry<String, List<String>> next : params.entrySet()) {
-                requestParams.put(next.getKey(), next.getValue().get(0));
-            }
-            request.setRequestParams(requestParams);
-            defaultFullHttpRequest= new DefaultFullHttpRequest(req.protocolVersion(),req.method(),req.uri(), Unpooled.buffer(0), req.headers(),req.trailingHeaders());
-        } else {
-            defaultFullHttpRequest= new DefaultFullHttpRequest(req.protocolVersion(),req.method(),req.uri(),Unpooled.copiedBuffer(req.content()), req.headers(),req.trailingHeaders());
-            HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(FACTORY, req);
-            List<InterfaceHttpData> bodyHttpDates = decoder.getBodyHttpDatas();
-            bodyHttpDates.forEach(request::writeHttpData);
-            decoder.destroy();
-            byte[] b = new byte[req.content().readableBytes()];
-            req.content().readBytes(b);
-            request.setBody(b);
-        }
-        request.setNettyRequest(defaultFullHttpRequest);
+        request.setNettyRequest(new DefaultFullHttpRequest(req.protocolVersion(), req.method(), req.uri(), Unpooled.copiedBuffer(req.content()), req.headers(), req.trailingHeaders()));
+        handlerUrl(request, req);
+        handlerBody(request, req);
         //获取URi，設置真實的URI
         int i = req.uri().indexOf("?");
         if (i > 0) {
@@ -73,12 +55,33 @@ public class HServerContentHandler extends SimpleChannelInboundHandler<FullHttpR
         webkit.httpRequest = hServerContext.getRequest();
         webkit.httpResponse = hServerContext.getResponse();
         hServerContext.setWebkit(webkit);
-
         channelHandlerContext.fireChannelRead(hServerContext);
     }
 
-  @Override
-  public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-    BuildResponse.writeException(ctx,cause);
-  }
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        BuildResponse.writeException(ctx, cause);
+    }
+
+
+    private void handlerUrl(Request request, FullHttpRequest req) {
+        Map<String, String> requestParams = new HashMap<>(1);
+        QueryStringDecoder decoder = new QueryStringDecoder(req.uri());
+        Map<String, List<String>> params = decoder.parameters();
+        for (Map.Entry<String, List<String>> next : params.entrySet()) {
+            requestParams.put(next.getKey(), next.getValue().get(0));
+        }
+        request.setRequestParams(requestParams);
+    }
+
+    private void handlerBody(Request request, FullHttpRequest req) {
+        HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(FACTORY, req);
+        List<InterfaceHttpData> bodyHttpDates = decoder.getBodyHttpDatas();
+        bodyHttpDates.forEach(request::writeHttpData);
+        decoder.destroy();
+        byte[] b = new byte[req.content().readableBytes()];
+        req.content().readBytes(b);
+        request.setBody(b);
+    }
+
 }
---
+++ src/test/java/test1/action/TestAction.java
@@ -0,0 +1,22 @@
+package test1.action;
+
+import top.hserver.core.interfaces.HttpRequest;
+import top.hserver.core.ioc.annotation.Controller;
+import top.hserver.core.ioc.annotation.GET;
+import top.hserver.core.ioc.annotation.POST;
+import top.hserver.core.server.util.JsonResult;
+
+@Controller
+public class TestAction {
+
+
+    @GET("/testGet")
+    public JsonResult get(HttpRequest request){
+        return  JsonResult.ok();
+    }
+
+    @POST("/testPost")
+    public JsonResult post(HttpRequest request){
+        return  JsonResult.ok();
+    }
+}
