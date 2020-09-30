2006762769986424f94c9628e4b7cc9cdfd5e837
424352644 <424352644@qq.com>
2020-09-30 17:29:49 +0800

    发布2.9.49 fix 控制器参数问题 拦截器静态文件问题

--- src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -35,7 +35,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.49";
+    public static final String VERSION = "2.9.50";
     /**
      * 定时任务线程数配置
      */
