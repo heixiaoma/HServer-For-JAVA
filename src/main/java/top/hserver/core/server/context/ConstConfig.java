9be264464eac169e8aad663be55d0badd71908e5
黑小马 <heixiaoma@gmail.com>
2020-10-06 15:23:00 +0800

    :zap: 发布2.9.50

--- src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -35,7 +35,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.50";
+    public static final String VERSION = "2.9.51";
     /**
      * 定时任务线程数配置
      */
