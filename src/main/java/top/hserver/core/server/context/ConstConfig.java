dc4e5efc1aa1928245f8507abca098dff8b3c2a4
424352644 <424352644@qq.com>
2020-10-22 18:19:27 +0800

    发布2.9.51

--- src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -35,7 +35,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.51";
+    public static final String VERSION = "2.9.52";
     /**
      * 定时任务线程数配置
      */
