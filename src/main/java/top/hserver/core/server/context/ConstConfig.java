26981d21177542c3dc5dd1080914a4b8a9ad25cd
黑小马 <heixiaoma@gmail.com>
2020-10-29 23:55:21 +0800

    :zap: 升级Netty版本 调整解码方式 发布2.9.52版本

--- src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -35,7 +35,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.52";
+    public static final String VERSION = "2.9.53";
     /**
      * 定时任务线程数配置
      */
