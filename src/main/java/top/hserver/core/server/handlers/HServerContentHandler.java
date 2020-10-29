44bca04e60f7b40908f41ccafeebb133e80c8364
黑小马 <heixiaoma@gmail.com>
2020-10-29 23:46:26 +0800

    :zap: 升级Netty版本 调整解码方式 发布2.9.52版本

--- src/main/java/top/hserver/core/server/handlers/HServerContentHandler.java
+++ src/main/java/top/hserver/core/server/handlers/HServerContentHandler.java
@@ -2,6 +2,7 @@ package top.hserver.core.server.handlers;
 
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.http.multipart.*;
+import org.apache.http.entity.ContentType;
 import top.hserver.core.server.context.*;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
@@ -9,6 +10,7 @@ import io.netty.handler.codec.http.*;
 import lombok.extern.slf4j.Slf4j;
 import top.hserver.core.server.util.HServerIpUtil;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -71,16 +73,19 @@ public class HServerContentHandler extends SimpleChannelInboundHandler<FullHttpR
         for (Map.Entry<String, List<String>> next : params.entrySet()) {
             requestParams.put(next.getKey(), next.getValue());
             for (String s : next.getValue()) {
-                request.addReqUrlParams(next.getKey(),s);
+                request.addReqUrlParams(next.getKey(), s);
             }
         }
     }
 
     private void handlerBody(Request request, FullHttpRequest req) {
-        HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(FACTORY, req);
-        List<InterfaceHttpData> bodyHttpDates = decoder.getBodyHttpDatas();
-        bodyHttpDates.forEach(request::writeHttpData);
-        decoder.destroy();
+        try {
+            HttpPostRequestDecoder decoder = new HttpPostRequestDecoder(FACTORY, req);
+            List<InterfaceHttpData> bodyHttpDates = decoder.getBodyHttpDatas();
+            bodyHttpDates.forEach(request::writeHttpData);
+            decoder.destroy();
+        } catch (Exception ignored) {
+        }
         byte[] b = new byte[req.content().readableBytes()];
         req.content().readBytes(b);
         request.setBody(b);
