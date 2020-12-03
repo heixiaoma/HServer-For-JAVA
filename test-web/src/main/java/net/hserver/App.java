b535333786a6696efeb35712e14d10a5fbd5c2f1
黑小马 <heixiaoma@gmail.com>
2020-12-03 20:49:04 +0800

    添加启动注解类

--- test-web/src/main/java/net/hserver/App.java
+++ test-web/src/main/java/net/hserver/App.java
@@ -1,10 +1,13 @@
 package net.hserver;
 
 import top.hserver.HServerApplication;
+import top.hserver.core.ioc.annotation.Controller;
+import top.hserver.core.ioc.annotation.HServerBoot;
 
 /**
  * @author hxm
  */
+@HServerBoot
 public class App {
 
     public static void main(String[] args) {
