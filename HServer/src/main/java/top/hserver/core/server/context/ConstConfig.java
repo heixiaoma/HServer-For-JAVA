574bc5464d8e1ab5c44fb52f682618db695422e1
424352644 <424352644@qq.com>
2020-12-30 11:47:18 +0800

    发布新版，支持队列持久化

--- HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -35,7 +35,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.57";
+    public static final String VERSION = "2.9.58";
     /**
      * 定时任务线程数配置
      */
