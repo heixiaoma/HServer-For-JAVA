b5a0f7decc01bdbeb11ad017a571dc44e595a62b
黑小马 <heixiaoma@gmail.com>
2021-04-01 10:25:58 +0800

    发布2.9.58 优化下载和Hook注解问题

--- HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -35,7 +35,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.58";
+    public static final String VERSION = "2.9.59";
     /**
      * 定时任务线程数配置
      */
