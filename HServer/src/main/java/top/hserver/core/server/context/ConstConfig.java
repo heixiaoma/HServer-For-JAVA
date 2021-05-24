12f24d92845b5c8882d07b0790a5a74910f312b0
黑小马 <heixiaoma@gmail.com>
2021-05-24 18:06:11 +0800

    fix 参数默认值 发布2.9.64

--- HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -36,7 +36,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.63";
+    public static final String VERSION = "2.9.64";
     /**
      * 定时任务线程数配置
      */
