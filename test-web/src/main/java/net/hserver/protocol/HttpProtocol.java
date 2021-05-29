169178f9f03ae681226600ad208c5d63153336b4
黑小马 <heixiaoma@gmail.com>
2021-05-29 10:17:50 +0800

    发布2.9.66

---
+++ test-web/src/main/java/net/hserver/protocol/HttpProtocol.java
@@ -0,0 +1,21 @@
+package net.hserver.protocol;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPipeline;
+import top.hserver.core.interfaces.ProtocolDispatcherAdapter;
+import top.hserver.core.ioc.annotation.Bean;
+import top.hserver.core.ioc.annotation.Order;
+import top.hserver.core.server.ServerInitializer;
+import top.hserver.core.server.dispatcher.DispatchHttp;
+
+/**
+ * @author hxm
+ */
+@Order(3)
+@Bean
+public class HttpProtocol extends DispatchHttp {
+    @Override
+    public boolean dispatcher(ChannelHandlerContext ctx, ChannelPipeline pipeline, byte[] headers, ServerInitializer.ProtocolDispatcher protocolDispatcher) {
+        return super.dispatcher(ctx, pipeline, headers, protocolDispatcher);
+    }
+}
