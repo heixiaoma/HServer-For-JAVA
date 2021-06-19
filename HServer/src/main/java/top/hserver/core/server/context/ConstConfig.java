5afc751a922cd171e681b7ea835a1a8a67bf9682
黑小马 <heixiaoma@gmail.com>
2021-06-19 17:30:25 +0800

    版本号

--- HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -36,7 +36,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.69";
+    public static final String VERSION = "2.9.70";
     /**
      * 定时任务线程数配置
      */
