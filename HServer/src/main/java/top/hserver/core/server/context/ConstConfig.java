634ebcae10f50eddafbd197688724bc3312e0219
黑小马 <heixiaoma@gmail.com>
2021-06-29 09:49:26 +0800

    优化队列

--- HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -36,7 +36,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.71";
+    public static final String VERSION = "2.9.72";
     /**
      * 定时任务线程数配置
      */
