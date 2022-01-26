2c7149c8cc3f99735a8e84e0636167d5fd4235e8
15228311770 <m135799>
2022-01-26 17:59:59 +0800

    添加日志扩展

---
+++ HServer/src/main/java/top/hserver/core/log/HServerLogAsyncAppender.java
@@ -0,0 +1,23 @@
+package top.hserver.core.log;
+
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+import top.hserver.core.interfaces.LogAdapter;
+import top.hserver.core.ioc.IocUtil;
+import java.util.List;
+
+public class HServerLogAsyncAppender extends AppenderBase<LoggingEvent> {
+    @Override
+    protected void append(LoggingEvent eventObject) {
+        List<LogAdapter> listBean = IocUtil.getListBean(LogAdapter.class);
+        if (listBean != null&&listBean.size()>0) {
+            for (LogAdapter logAdapter : listBean) {
+                try {
+                    logAdapter.log(eventObject);
+                }catch (Throwable e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
