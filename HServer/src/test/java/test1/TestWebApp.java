1f82687ff0f54c02f83f1172afb658626919600b
15228311770 <m135799>
2022-01-26 15:41:02 +0800

    公开请求数

--- HServer/src/test/java/test1/TestWebApp.java
+++ HServer/src/test/java/test1/TestWebApp.java
@@ -1,12 +1,12 @@
 package test1;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import top.hserver.core.test.HServerTestServer;
+import top.hserver.core.test.HServerTest;
 
 /**
  * @author hxm
  */
-@RunWith(HServerTestServer.class)
+@RunWith(HServerTest.class)
 public class TestWebApp {
 
   @Test
