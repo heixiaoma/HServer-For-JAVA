f6deca79a37e7c11630e4c37f43bf2c851888f24
15228311770 <m135799>
2022-01-28 17:52:23 +0800

    发布新版

--- HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -43,7 +43,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.81";
+    public static final String VERSION = "2.9.82";
     /**
      * 定时任务线程数配置
      */
