5f16c635c4f6de674a297ad5b3e967f90230fa25
hxm <heixiaoma@gmail.com>
2022-02-09 11:00:00 +0800

    异常日志，不进入标准错误输出流

--- HServer/src/main/java/top/hserver/core/server/util/SpanUtil.java
+++ HServer/src/main/java/top/hserver/core/server/util/SpanUtil.java
@@ -1,16 +1,20 @@
 package top.hserver.core.server.util;
 
 import com.alibaba.ttl.TransmittableThreadLocal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import top.hserver.HServerApplication;
 
 import java.util.Stack;
 
 public class SpanUtil {
+    private static final Logger log = LoggerFactory.getLogger(SpanUtil.class);
 
     private static final SnowflakeIdWorker SNOWFLAKE_ID_WORKER=new SnowflakeIdWorker(1,30);
     private static final TransmittableThreadLocal<Stack<Long>> threadMethods = new TransmittableThreadLocal<>();
 
     public static void add() {
-        Stack<Long> queue = null;
+        Stack<Long> queue;
         if (null == threadMethods.get()) {
             queue = new Stack<>();
             queue.add(SNOWFLAKE_ID_WORKER.nextId());
@@ -29,6 +33,7 @@ public class SpanUtil {
             }
             return integers.peek();
         }catch (Exception e){
+            log.error(ExceptionUtil.getMessage(e));
             return -1;
         }
     }
