59c391e3cd639bca490a1000adc11f0fdb086ab3
hxm <heixiaoma@gmail.com>
2022-03-23 13:21:13 +0800

    整理代码

--- HServer/src/main/java/top/hserver/core/server/util/TTLUtil.java
+++ HServer/src/main/java/top/hserver/core/server/util/TTLUtil.java
@@ -1,22 +1,8 @@
 package top.hserver.core.server.util;
 
-import com.alibaba.ttl.threadpool.TtlExecutors;
-import io.netty.channel.DefaultSelectStrategyFactory;
-import io.netty.channel.EventLoop;
 import io.netty.channel.EventLoopGroup;
-import io.netty.channel.SelectStrategyFactory;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.util.concurrent.*;
-
-import java.nio.channels.spi.SelectorProvider;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executor;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-
-
 public class TTLUtil {
     public static EventLoopGroup getEventLoop(int size, String name) {
         if (EpollUtil.check()) {
