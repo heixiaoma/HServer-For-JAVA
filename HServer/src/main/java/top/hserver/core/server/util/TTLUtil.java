d9d24ab2f00a6166aba891d173b3bfeee7b810dd
hxm <heixiaoma@gmail.com>
2022-04-06 10:16:22 +0800

    优化

--- HServer/src/main/java/top/hserver/core/server/util/TTLUtil.java
+++ HServer/src/main/java/top/hserver/core/server/util/TTLUtil.java
@@ -3,7 +3,9 @@ package top.hserver.core.server.util;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.epoll.EpollEventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+
 public class TTLUtil {
+
     public static EventLoopGroup getEventLoop(int size, String name) {
         if (EpollUtil.check()) {
             return new EpollEventLoopGroup(size, new NamedThreadFactory(name));
