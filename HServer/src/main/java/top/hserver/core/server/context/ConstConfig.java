b9c2085836d289aab4096971655fcca387ffa452
hxm <heixiaoma@gmail.com>
2022-06-09 20:49:25 +0800

    发布新版

--- HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
+++ HServer/src/main/java/top/hserver/core/server/context/ConstConfig.java
@@ -35,7 +35,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "2.9.90";
+    public static final String VERSION = "2.9.91";
     /**
      * 定时任务线程数配置
      */
