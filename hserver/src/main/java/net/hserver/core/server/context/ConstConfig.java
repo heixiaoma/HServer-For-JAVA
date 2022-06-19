c79fe4c90e5d485fdbb7d6af68de3d5c148b12a4
hxm <heixiaoma@gmail.com>
2022-06-19 17:48:51 +0800

    优化项目依赖

--- hserver/src/main/java/net/hserver/core/server/context/ConstConfig.java
+++ hserver/src/main/java/net/hserver/core/server/context/ConstConfig.java
@@ -32,7 +32,7 @@ public class ConstConfig {
     /**
      * 版本号
      */
-    public static final String VERSION = "3.0-M1";
+    public static final String VERSION = "3.0.M1";
     /**
      * 定时任务线程数配置
      */
