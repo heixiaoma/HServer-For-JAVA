206b5ff90fa88df6bd3a0fb1d8e444705671087c
黑小马 <Mcl135799>
2022-07-20 11:09:47 +0800

    重构HServer所有模块服务器插件化

---
+++ hserver-plugin-web/src/main/java/cn/hserver/plugin/web/WebPlugin.java
@@ -0,0 +1,25 @@
+package cn.hserver.plugin.web;
+
+import cn.hserver.core.interfaces.PluginAdapter;
+
+public class WebPlugin implements PluginAdapter {
+    @Override
+    public void startIocInit() {
+
+    }
+
+    @Override
+    public void iocInitEnd() {
+
+    }
+
+    @Override
+    public void startInjection() {
+
+    }
+
+    @Override
+    public void injectionEnd() {
+
+    }
+}
