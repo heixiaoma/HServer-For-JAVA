c368cead18937222331317ed9e2db93f12cd10ec
黑小马 <Mcl135799>
2022-08-22 14:21:57 +0800

    新版迭代准备

--- hserver/src/main/java/cn/hserver/core/server/context/ConstConfig.java
+++ hserver/src/main/java/cn/hserver/core/server/context/ConstConfig.java
@@ -64,7 +64,7 @@ public class ConstConfig {
      */
     public final static String SERVER_NAME = "HServer";
 
-    public final static String VERSION = "3.0.0";
+    public final static String VERSION = "3.1.M1";
 
     /**
      * 用户自定义的服务名
