3ecfa2cd643a534b57ae24ff72c2e6396b860343
hxm <heixiaoma@gmail.com>
2022-09-11 10:33:47 +0800

    代理对象bugfix

--- hserver/src/main/java/cn/hserver/core/ioc/HServerIoc.java
+++ hserver/src/main/java/cn/hserver/core/ioc/HServerIoc.java
@@ -34,7 +34,7 @@ public class HServerIoc implements Ioc {
                 List o1 = (List) o;
                 for (int i = 0; i < o1.size(); i++) {
                     //如果子类存在，父类直接跳过不被加入
-                    if (bean.getClass().isAssignableFrom(o1.get(i).getClass())&&!ProxyObject.class.isAssignableFrom(o1.get(i).getClass())) {
+                    if (bean.getClass().isAssignableFrom(o1.get(i).getClass())||ProxyObject.class.isAssignableFrom(o1.get(i).getClass())) {
                         return;
                     }
                     //如果这个类是List中的子类，我们优先使用子类重写父类功能
@@ -110,9 +110,11 @@ public class HServerIoc implements Ioc {
         if (name != null && name.trim().length() > 0 && bean != null) {
 
             Object bean1 = getBean(name);
-            //如果子类存在，父类直接跳过不被加入 同时不是 代理类
-            if (bean.getClass().isAssignableFrom(bean1.getClass())&&!ProxyObject.class.isAssignableFrom(bean1.getClass())) {
-                return;
+            if (bean1!=null) {
+                //如果子类存在，父类直接跳过不被加入 同时不是 代理类
+                if (bean.getClass().isAssignableFrom(bean1.getClass()) || ProxyObject.class.isAssignableFrom(bean1.getClass())) {
+                    return;
+                }
             }
             POOL.put(name, bean);
         }
