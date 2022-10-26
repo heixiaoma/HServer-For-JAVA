000f0ccbb98b39124bc316ac87a82b7fcb2625dc
hxm <heixiaoma@gmail.com>
2022-10-26 19:32:58 +0800

    gzip和ssl问题

--- hserver-plugin-gateway/src/main/java/cn/hserver/plugin/gateway/handler/http7/Http7FrontendHandler.java
+++ hserver-plugin-gateway/src/main/java/cn/hserver/plugin/gateway/handler/http7/Http7FrontendHandler.java
@@ -4,29 +4,32 @@ import cn.hserver.core.ioc.IocUtil;
 import cn.hserver.plugin.gateway.business.Business;
 import cn.hserver.plugin.gateway.business.BusinessHttp7;
 import cn.hserver.plugin.gateway.business.BusinessTcp;
+import cn.hserver.plugin.gateway.ssl.HttpsMapperSslContextFactory;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.HttpClientCodec;
-import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.ssl.SslHandler;
 import io.netty.handler.stream.ChunkedWriteHandler;
 import io.netty.util.ReferenceCountUtil;
+
+import javax.net.ssl.SSLEngine;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 
 
 public class Http7FrontendHandler extends ChannelInboundHandlerAdapter {
 
     private Channel outboundChannel;
 
-    private  static BusinessHttp7 businessHttp7;
+    private static BusinessHttp7 businessHttp7;
 
     public Http7FrontendHandler() {
         for (Business business : IocUtil.getListBean(Business.class)) {
             if (business instanceof BusinessHttp7) {
-                businessHttp7 = (BusinessHttp7)business;
+                businessHttp7 = (BusinessHttp7) business;
             }
         }
     }
@@ -50,7 +53,7 @@ public class Http7FrontendHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(final ChannelHandlerContext ctx, Object msg) {
         //数据入场
-        Object in = businessHttp7.in(ctx,msg);
+        Object in = businessHttp7.in(ctx, msg);
         if (in == null) {
             return;
         }
@@ -58,15 +61,21 @@ public class Http7FrontendHandler extends ChannelInboundHandlerAdapter {
             final Channel inboundChannel = ctx.channel();
             Bootstrap b = new Bootstrap();
             b.group(ctx.channel().eventLoop());
+            InetSocketAddress proxyHost = (InetSocketAddress) businessHttp7.getProxyHost(ctx, in, ctx.channel().localAddress());
+            SSLEngine sslEngine = HttpsMapperSslContextFactory.getClientContext().createSSLEngine();
+            sslEngine.setUseClientMode(true);
             b.channel(NioSocketChannel.class).handler(new ChannelInitializer<Channel>() {
                 @Override
                 protected void initChannel(Channel ch) {
-                    ch.pipeline().addLast(new HttpClientCodec(), new HttpObjectAggregator(Integer.MAX_VALUE));
-                    ch.pipeline().addLast(new Http7BackendHandler(inboundChannel,businessHttp7));
+                    if (proxyHost.getPort() == 443) {
+                        ch.pipeline().addFirst(new SslHandler(sslEngine));
+                    }
+                    ch.pipeline().addLast(new HttpClientCodec(),new HttpContentDecompressor(), new HttpObjectAggregator(Integer.MAX_VALUE));
+                    ch.pipeline().addLast(new Http7BackendHandler(inboundChannel, businessHttp7));
                 }
             });
             //数据代理服务选择器
-            ChannelFuture f = b.connect(businessHttp7.getProxyHost(ctx,in,ctx.channel().localAddress())).addListener((ChannelFutureListener) future -> {
+            ChannelFuture f = b.connect(proxyHost).addListener((ChannelFutureListener) future -> {
                 if (future.isSuccess()) {
                     future.channel().writeAndFlush(in);
                 } else {
