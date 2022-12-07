abcd4ba1a0e3ca8f51fddcd30c2413250d2df785
黑小马 <heixiaoma@gmail.com>
2022-12-07 13:38:19 +0800

    添加对localdate等序列化问题

--- hserver-plugin-web/src/main/java/cn/hserver/plugin/web/context/WebConstConfig.java
+++ hserver-plugin-web/src/main/java/cn/hserver/plugin/web/context/WebConstConfig.java
@@ -3,6 +3,7 @@ package cn.hserver.plugin.web.context;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.netty.handler.ssl.SslContext;
 import io.netty.util.concurrent.EventExecutorGroup;
 import cn.hserver.plugin.web.json.JackSonJsonAdapter;
@@ -32,6 +33,7 @@ public class WebConstConfig {
      * 对象处理
      */
     public static final ObjectMapper OBJECT_MAPPER = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
             .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
             .configure(FAIL_ON_UNKNOWN_PROPERTIES, false)
             .configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true)
