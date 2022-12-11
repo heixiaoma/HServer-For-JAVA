613fc022b6ffb3a584c3c86050997a0e51457462
hxm <heixiaoma@gmail.com>
2022-12-11 14:41:08 +0800

    添加堆外内存打印

--- hserver/src/main/java/cn/hserver/core/server/util/JvmStack.java
+++ hserver/src/main/java/cn/hserver/core/server/util/JvmStack.java
@@ -1,5 +1,10 @@
 package cn.hserver.core.server.util;
 
+import cn.hserver.HServerApplication;
+import io.netty.util.internal.PlatformDependent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
@@ -9,44 +14,53 @@ import java.util.List;
 
 public class JvmStack {
 
+    private static final Logger log = LoggerFactory.getLogger(JvmStack.class);
+
+
     private static final long MB = 1048576L;
 
     public static void printMemoryInfo() {
         MemoryMXBean memory = ManagementFactory.getMemoryMXBean();
         MemoryUsage headMemory = memory.getHeapMemoryUsage();
 
-        String info = String.format("\ninit: %s\t max: %s\t used: %s\t committed: %s\t use rate: %s\n",
+        String info = String.format("\n初始: %s\t 最大: %s\t 已经使用: %s\t 交给虚拟机使用的: %s\t 使用率: %s\n",
                 headMemory.getInit() / MB + "MB",
                 headMemory.getMax() / MB + "MB", headMemory.getUsed() / MB + "MB",
                 headMemory.getCommitted() / MB + "MB",
                 headMemory.getUsed() * 100 / headMemory.getCommitted() + "%"
 
         );
-
-        System.out.print(info);
-
+        log.info(info);
         MemoryUsage nonheadMemory = memory.getNonHeapMemoryUsage();
 
-        info = String.format("init: %s\t max: %s\t used: %s\t committed: %s\t use rate: %s\n",
+        info = String.format("初始: %s\t 最大: %s\t 已经使用: %s\t 交给虚拟机使用的: %s\t  使用率: %s\n",
                 nonheadMemory.getInit() / MB + "MB",
                 nonheadMemory.getMax() / MB + "MB", nonheadMemory.getUsed() / MB + "MB",
                 nonheadMemory.getCommitted() / MB + "MB",
                 nonheadMemory.getUsed() * 100 / nonheadMemory.getCommitted() + "%"
 
         );
-        System.out.println(info);
+        log.info(info);
 
+        //打印堆外内存
+        long maxDirectMemory = PlatformDependent.maxDirectMemory();
+        long usedDirectMemory = PlatformDependent.usedDirectMemory();
+        info = String.format("堆外内存 最大: %s\t 已经使用: %s\n",
+                maxDirectMemory / MB + "MB",
+                usedDirectMemory / MB + "MB"
+        );
+        log.info(info);
     }
 
     public static void printGCInfo() {
         List<GarbageCollectorMXBean> garbages = ManagementFactory.getGarbageCollectorMXBeans();
         for (GarbageCollectorMXBean garbage : garbages) {
-            String info = String.format("name: %s\t count:%s\t took:%s\t pool name:%s",
+            String info = String.format("名字: %s\t 次数:%s\t 搜集时间:%s\t 内存池名字:%s",
                     garbage.getName(),
                     garbage.getCollectionCount(),
                     garbage.getCollectionTime(),
                     Arrays.deepToString(garbage.getMemoryPoolNames()));
-            System.out.println(info);
+            log.info(info);
         }
     }
 
