6557b0ec6f153fb6f05e64af426fadf4c3a98863
黑小马 <heixiaoma@gmail.com>
2022-12-14 12:54:43 +0800

    对微服务设计

---
+++ hserver-plugin-nacos/src/main/java/cn/hserver/plugin/nacos/StartRegServer.java
@@ -0,0 +1,10 @@
+package cn.hserver.plugin.nacos;
+
+import cn.hserver.core.interfaces.InitRunner;
+
+public class StartRegServer implements InitRunner {
+    @Override
+    public void init(String[] args) {
+
+    }
+}
