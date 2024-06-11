dd7f9a292bfa8d69dbf6dcc81caaa20a25c6e1f3
黑小马 <heixiaoma@gmail.com>
2024-06-11 18:27:38 +0800

    添加Satoken插件

--- hserver-plugin-satoken/src/main/java/cn/hserver/plugin/satoken/SaTokenPlugin.java
+++ hserver-plugin-satoken/src/main/java/cn/hserver/plugin/satoken/SaTokenPlugin.java
@@ -4,7 +4,12 @@ import cn.dev33.satoken.SaManager;
 import cn.dev33.satoken.config.SaTokenConfig;
 import cn.dev33.satoken.config.SaTokenConfigFactory;
 import cn.dev33.satoken.context.SaTokenContext;
+import cn.dev33.satoken.dao.SaTokenDao;
+import cn.dev33.satoken.json.SaJsonTemplate;
+import cn.dev33.satoken.same.SaSameTemplate;
+import cn.dev33.satoken.sign.SaSignTemplate;
 import cn.dev33.satoken.stp.StpInterface;
+import cn.dev33.satoken.temp.SaTempInterface;
 import cn.hserver.core.interfaces.PluginAdapter;
 import cn.hserver.core.ioc.IocUtil;
 import cn.hserver.core.ioc.ref.PackageScanner;
@@ -52,7 +57,34 @@ public class SaTokenPlugin implements PluginAdapter {
     public void injectionEnd() {
         SaTokenContext saTokenContext = new SaTokenContextForHServer();
         SaManager.setSaTokenContext(saTokenContext);
-        SaManager.setStpInterface(IocUtil.getSupperBean(StpInterface.class));
+        StpInterface stpInterface = IocUtil.getSupperBean(StpInterface.class);
+        if (stpInterface != null) {
+            SaManager.setStpInterface(stpInterface);
+        }
+        SaTokenDao saTokenDao = IocUtil.getSupperBean(SaTokenDao.class);
+        if (saTokenDao != null) {
+            SaManager.setSaTokenDao(saTokenDao);
+        }
+
+        SaSignTemplate saSignTemplate = IocUtil.getSupperBean(SaSignTemplate.class);
+        if (saSignTemplate != null){
+            SaManager.setSaSignTemplate(saSignTemplate);
+        }
+
+        SaTempInterface saTempInterface = IocUtil.getSupperBean(SaTempInterface.class);
+        if (saTempInterface != null){
+            SaManager.setSaTemp(saTempInterface);
+        }
+        SaJsonTemplate saJsonTemplate = IocUtil.getSupperBean(SaJsonTemplate.class);
+        if (saJsonTemplate != null){
+            SaManager.setSaJsonTemplate(saJsonTemplate);
+        }
+
+        SaSameTemplate saSameTemplate = IocUtil.getSupperBean(SaSameTemplate.class);
+        if (saSameTemplate != null){
+            SaManager.setSaSameTemplate(saSameTemplate);
+        }
+
         log.info("Sa-Token插件启动成功");
     }
 }
