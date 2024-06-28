3af9a1f31ce83bc996f113fd844d05e8d18525b2
黑小马 <heixiaoma@gmail.com>
2024-06-28 10:09:01 +0800

    1

--- hserver/src/main/java/cn/hserver/core/queue/timer/QueueBootstrap.java
+++ hserver/src/main/java/cn/hserver/core/queue/timer/QueueBootstrap.java
@@ -6,12 +6,13 @@ import java.util.concurrent.TimeUnit;
 public class QueueBootstrap {
 	private final ScheduledExecutorService newScheduledThreadPool = Executors.newScheduledThreadPool(1);
 
-	public void start() {
+	public WheelQueue start() {
 		WheelQueue wheelQueue = new WheelQueue();
 		// 定义任务
 		QueueScanTimer timerTask = new QueueScanTimer(wheelQueue);
 		// 设置任务的执行，1秒后开始，每1秒执行一次
 		newScheduledThreadPool.scheduleWithFixedDelay(timerTask, 1, 1, TimeUnit.SECONDS);
+		return wheelQueue;
 	}
 	/**
 	 * 停止此队列运行。
