91080d23d418320ce84a8fd95b8850c47d19cf96
黑小马 <heixiaoma@gmail.com>
2024-11-19 17:50:45 +0800

    发布新版

--- hserver/src/main/java/cn/hserver/core/server/context/ConstConfig.java
+++ hserver/src/main/java/cn/hserver/core/server/context/ConstConfig.java
@@ -59,7 +59,7 @@ public class ConstConfig {
      */
     public final static String SERVER_NAME = "HServer";
 
-    public final static String VERSION = "3.6.M1";
+    public final static String VERSION = "3.6.M2";
 
     /**
      * 用户自定义的服务名
