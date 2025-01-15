e27c1d5aacebf104f9da216ce0cad27c33972f5c
黑小马 <heixiaoma@gmail.com>
2025-01-15 23:43:21 +0800

    切换logbak

--- hserver/src/main/java/cn/hserver/core/log/HServerLogConfig.java
+++ hserver/src/main/java/cn/hserver/core/log/HServerLogConfig.java
@@ -6,15 +6,6 @@ import ch.qos.logback.classic.LoggerContext;
 import cn.hserver.core.ioc.IocUtil;
 import cn.hserver.core.server.context.ServerConfig;
 import cn.hserver.core.server.util.ExceptionUtil;
-import cn.hserver.core.server.util.PropUtil;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.Layout;
-import org.apache.logging.log4j.core.appender.ConsoleAppender;
-import org.apache.logging.log4j.core.layout.PatternLayout;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.DefaultConfiguration;
 import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
@@ -41,27 +32,12 @@ public class HServerLogConfig {
         }
     }
 
+    private static void stopAndReset(LoggerContext loggerContext) {
+        loggerContext.stop();
+        loggerContext.reset();
+    }
+
     private static void loadConfiguration(InputStream in) throws Exception {
-        // 配置日志输出格式（包含高亮）
-        Layout layout = PatternLayout.newBuilder()
-                .withPattern(pattern)
-                .build();
-
-        // 创建 ConsoleAppender 并设置布局
-        Appender consoleAppender = ConsoleAppender.newBuilder()
-                .setLayout(layout)
-                .setTarget(ConsoleAppender.Target.SYSTEM_OUT)
-                .build();
-
-        // 将 Appender 添加到配置
-        DefaultConfiguration configuration = new DefaultConfiguration();
-        configuration.addAppender(consoleAppender);
-        Configurator.initialize(configuration);
-
-        // 设置日志级别
-        Configurator.setRootLevel(org.apache.logging.log4j.Level.DEBUG);
-
-
         LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
         stopAndReset(loggerContext);
         ch.qos.logback.classic.joran.JoranConfigurator configurator = new ch.qos.logback.classic.joran.JoranConfigurator();
